4998d45413e048da08ceb2e84f308aa1355d3bf1
Liam Briggs <liamandrewbriggs@gmail.com>
2018-01-29 14:54:25 +0000

    Created new package
    Whilst working on the Sensor Monitor

---
+++ src/SmartCity/SensorMonitor.java
@@ -0,0 +1,32 @@
+package SmartCity;
+
+/**
+ *
+ * @author User
+ */
+import java.util.*;
+
+public class SensorMonitor {
+    
+    private String sensorMonitorID;
+    private ArrayList<Double> coords = new ArrayList<>();
+    private Boolean isActive;
+    private Double interval;
+    private SensorStation observer;
+    private Long lastReadingTime;
+    private Sensor sensor;
+    public Integer readingsCount;
+    public Data reading;
+    
+    
+    public SensorMonitor()
+    {
+        
+    }
+    
+    public void pollData()
+    {
+        
+    }
+    
+}
