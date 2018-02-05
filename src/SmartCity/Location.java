fc57e6129163db6012f1350c6b8a06bd30b4b240
DranAlexander <34461595+DranAlexander@users.noreply.github.com>
2018-02-05 13:56:24 +0000

    Classes created
    SensorStation, Sensor, Location and Data classes created.

---
+++ src/SmartCity/Location.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SmartCity;
+
+/**
+ *
+ * @author Dran
+ */
+public class Location {
+    
+    private double latitude;
+    private double longitude;
+    
+    public getCoords() {
+        
+    }
+    
+    public toAddress(){
+        
+    }
+    
+}
